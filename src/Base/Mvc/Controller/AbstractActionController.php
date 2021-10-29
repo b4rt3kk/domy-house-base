c11ff65e061a48dff4420be21c3f19a7c433f145
bwiechnik <bartlomiej.wiechnik@gmail.com>
2021-10-29 02:37:56 +0200

    Poprawka sprawdzania uprawnien RBAC w kontrolerze

--- src/Base/Mvc/Controller/AbstractActionController.php
+++ src/Base/Mvc/Controller/AbstractActionController.php
@@ -10,6 +10,38 @@ abstract class AbstractActionController extends \Laminas\Mvc\Controller\Abstract
         $this->setServiceManager($serviceManager);
     }
     
+    public function onDispatch(\Laminas\Mvc\MvcEvent $e)
+    {
+        $application = $e->getApplication();
+        $serviceManager = $application->getServiceManager();
+        
+        $controller = $e->getTarget();
+        $routeName = $e->getRouteMatch()->getMatchedRouteName();
+        $actionName = $e->getRouteMatch()->getParam('action', null);
+        
+        $authManager = $serviceManager->get(\Base\Services\Auth\AuthManager::class);
+        /* @var $authManager \Base\Services\Auth\AuthManager */
+
+        // sprawdzenie czy użytkownik ma dostęp do zasobu
+        $result = $authManager->filterAccess($routeName, $actionName);
+        
+        switch ($result) {
+            case \Base\Services\Auth\AuthManager::ACCESS_DENIED:
+                if ($routeName !== 'auth' || $actionName !== 'notauthorized') {
+                    return $controller->redirect()->toRoute('auth', ['action' => 'notauthorized']);
+                }
+                break;
+            case \Base\Services\Auth\AuthManager::AUTH_REQUIRED:
+                if ($routeName !== 'auth' || $actionName !== 'login') {
+                    $controller->flashMessenger()->addErrorMessage('Musisz się zalogować by uzyskać dostęp do tego zasobu');
+
+                    return $controller->redirect()->toRoute('auth', ['action' => 'login']);
+                }
+                break;
+        }
+
+        return parent::onDispatch($e);
+    }
     
     /**
      * @return \Laminas\ServiceManager\ServiceManager
