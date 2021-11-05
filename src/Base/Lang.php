a292309f55a3fbfd1eb963ebc06538962cf9ca69
bwiechnik <bartlomiej.wiechnik@gmail.com>
2021-11-06 00:47:03 +0100

    Dodanie Singleton dla Lang

---
+++ src/Base/Lang.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Base;
+
+class Lang extends \Base\Logic\AbstractLogic
+{
+    protected static $instance;
+    
+    protected $defaultLanguageSymbol;
+    
+    protected $currentLanguageSymbol;
+    
+    /**
+     * @return \Base\Lang
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance instanceof Lang) {
+            self::$instance = new Lang();
+        }
+        
+        return self::$instance;
+    }
+    
+    public function getDefaultLanguageSymbol()
+    {
+        return $this->defaultLanguageSymbol;
+    }
+
+    public function getCurrentLanguageSymbol()
+    {
+        $languageSymbol = $this->currentLanguageSymbol;
+        
+        if (empty($languageSymbol)) {
+            $languageSymbol = $this->getDefaultLanguageSymbol();
+        }
+        
+        return $languageSymbol;
+    }
+
+    public function setDefaultLanguageSymbol($defaultLanguageSymbol)
+    {
+        $this->defaultLanguageSymbol = $defaultLanguageSymbol;
+    }
+
+    public function setCurrentLanguageSymbol($currentLanguageSymbol)
+    {
+        $this->currentLanguageSymbol = $currentLanguageSymbol;
+    }
+}
