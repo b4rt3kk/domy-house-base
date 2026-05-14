600211f75da8226952efb79d9bf83a69c37f2ee3
bwiechnik <bartlomiej.wiechnik@gmail.com>
2026-05-14 22:15:23 +0200

    Poprawka sprawdzania uprawnień w RbacRolesManager dla permissions z wildcard

--- src/Base/Services/Rbac/RbacRolesManager.php
+++ src/Base/Services/Rbac/RbacRolesManager.php
@@ -6,11 +6,11 @@ use Base\Logic\AbstractLogic;
 abstract class RbacRolesManager extends AbstractLogic
 {
     protected $roleNameColumn = 'code';
-    
+
     protected $permissionNameColumn = 'name';
-    
+
     protected $primaryKey = 'id';
-    
+
     public function setRoleNameColumn($roleNameColumn)
     {
         $this->roleNameColumn = $roleNameColumn;
@@ -29,7 +29,7 @@ abstract class RbacRolesManager extends AbstractLogic
     {
         return $this->roleNameColumn;
     }
-    
+
     /**
      * Pobierz nazwę klucza głównego dla roli
      * @return string
@@ -38,7 +38,7 @@ abstract class RbacRolesManager extends AbstractLogic
     {
         return $this->primaryKey;
     }
-    
+
     /**
      * Pobierz nazwę kolumny, w której przechowywana jest nazwa kolumny dla uprawnienia
      * @return string
@@ -52,7 +52,7 @@ abstract class RbacRolesManager extends AbstractLogic
     {
         $this->permissionNameColumn = $permissionNameColumn;
     }
-    
+
     /**
      * Pobierz listę uprawnień dla roli, które posiadają parametr * oznaczający dowolną wartość
      * @param integer $idRole
@@ -62,23 +62,23 @@ abstract class RbacRolesManager extends AbstractLogic
     {
         $dataRolePermissions = $this->getRolePermissionsData($idRole);
         $permissionColumnName = $this->getPermissionNameColumn();
-        
+
         $resultSet = new \Laminas\Db\ResultSet\ResultSet();
         $resultSet->setArrayObjectPrototype($dataRolePermissions->getArrayObjectPrototype());
-        
+
         $foundPermissions = [];
-        
+
         foreach ($dataRolePermissions as $rowRolePermission) {
             if (strpos($rowRolePermission->{$permissionColumnName}, RbacManager::ANY_PARAM) !== false) {
-                $foundPermissions[] = clone $rowRolePermission;
+                $foundPermissions[] = $rowRolePermission;
             }
         }
-        
+
         $resultSet->initialize(new \ArrayIterator($foundPermissions));
-        
+
         return $resultSet;
     }
-    
+
     /**
      * Sprawdź czy rola posiada uprawnienia zawierające \Base\Services\Rbac\RbacManager::ANY_PARAM w swojej treści
      * @param integer $idRole
@@ -87,67 +87,72 @@ abstract class RbacRolesManager extends AbstractLogic
     public function hasAnyPermissions($idRole)
     {
         $data = $this->getAnyPermissionsData($idRole);
-        
+
         return $data->count() > 0;
     }
-    
+
     public function isAnyPermissionAllowed($idRole, $permission)
     {
         if (!$this->hasAnyPermissions($idRole)) {
             // rola nie posiada uprawnień zawierających \Base\Services\Rbac\RbacManager::ANY_PARAM
             return false;
         }
-        
+
         $data = $this->getAnyPermissionsData($idRole);
         $permissionNameColumn = $this->getPermissionNameColumn();
-        
+
         foreach ($data as $row) {
             $permissionName = preg_quote($row->{$permissionNameColumn});
-            
+
+            // pominięcie pustych wpisów
+            if (empty($permissionName)) {
+                continue;
+            }
+
             $pattern = '#' . str_replace('\\' . RbacManager::ANY_PARAM, '.*', $permissionName) . '#';
-            
-            if (preg_match($pattern, $permission) !== false) {
-               return true;
+
+            if (preg_match($pattern, $permission) === 1) {
+                return true;
             }
         }
-        
+
         return false;
     }
-        
+
     /**
      * Pobierz listę wszystkich dostępnych ról
      * @return \Laminas\Db\ResultSet\ResultSet
      */
     abstract public function getRolesData();
-    
+
     /**
      * Pobierz listę rodziców dla roli o id podanym w parametrze
      * @param integer $idRole
      * @return array
      */
     abstract public function getRoleParentsNames($idRole);
-    
+
     /**
      * Pobierz listę dzieci dla roli o id podanym w parametrze
      * @param integer $idRole
      * @return array
      */
     abstract public function getRoleChildrensNames($idRole);
-    
+
     /**
      * Pobierz listę uprawnień przypisanych do roli o id podanym w parametrze
      * @param integer $idRole
      * @return \Laminas\Db\ResultSet\ResultSet
      */
     abstract public function getRolePermissionsData($idRole);
-    
+
     /**
      * Pobierz listę ról podpiętych do wybranego użytkownika
      * @param integer $idUser
      * @return \Laminas\Db\ResultSet\ResultSet
      */
     abstract public function getUserRolesData($idUser);
-    
+
     /**
      * Pobierz wiersz dla roli na podstawie jego nazwy
      * @param string $name
